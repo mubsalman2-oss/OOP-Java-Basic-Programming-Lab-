f578b9d02aa7677d8dc1b02cad1ff3c46240a02f
mubsalman2-oss <mubsalman2@gmail.com>
2026-08-06 17:12:08 +0600

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int number = scanner.nextInt();
+        System.out.println(number + " * 1 = " + (number * 1));
+        System.out.println(number + " * 2 = " + (number * 2));
+        System.out.println(number + " * 3 = " + (number * 3));
+        System.out.println(number + " * 4 = " + (number * 4));
+        System.out.println(number + " * 5 = " + (number * 5));
+        System.out.println(number + " * 6 = " + (number * 6));
+        System.out.println(number + " * 7 = " + (number * 7));
+        System.out.println(number + " * 8 = " + (number * 8));
+        System.out.println(number + " * 9 = " + (number * 9));
+        System.out.println(number + " * 10 = " + (number * 10));
+        scanner.close();
+    }
+}
